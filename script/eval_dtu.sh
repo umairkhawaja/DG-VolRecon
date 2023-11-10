cec96fbe2dbcbed8f5b9e9ab09d438ffde56e31e
Khawaja Umair <khawaja.umair@tum.de>
2023-11-10 12:25:19 +0100

    Update gitignore and eval script

--- script/eval_dtu.sh
+++ script/eval_dtu.sh
@@ -3,9 +3,10 @@
 
 DATASET="/home/dataset/DTU_TEST/"
 
-LOAD_CKPT="checkpoints/epoch=15-step=193199.ckpt" 
+# LOAD_CKPT="checkpoints/epoch=15-step=193199.ckpt" # Baseline
+LOAD_CKPT="checkpoints/lightning_logs/xe9a4rud/checkpoints/epoch=1-step=48300.ckpt" # Your own weights
 
-OUT_DIR="./pretrained_outputs"
+OUT_DIR="./eval_outputs/test/"
 
 python main.py --extract_geometry --set 0 \
 --test_n_view 3 --test_ray_num 400 --volume_reso 96 \
