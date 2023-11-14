a5f228a3897ef14a6f91d71473c685b214c6596d
Sibo Wu <diana_wu2000@163.com>
2023-11-14 20:31:00 +0100

    dataloader supports reading depth prior for training

--- script/train_dtu.sh
+++ script/train_dtu.sh
@@ -4,7 +4,7 @@ DATASET="./dtu_training/"
 
 LOG_DIR="./checkpoints"
 
-python main.py --max_epochs 16 --batch_size 2 --lr 0.0001 \
+python main.py --max_epochs 16 --batch_size 1 --lr 0.0001 \
 --weight_rgb 1.0 --weight_depth 1.0 \
 --train_ray_num 1024 --volume_reso 96 \
 --root_dir=$DATASET --logdir=$LOG_DIR $@
